89f3c166babad3c34404ff629b51e5ac68d6e57e
Rohit <rohitkhatri111112@gmail.com>
2024-05-16 01:24:36 +0530

    Commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-import dotenv from "dotenv"
-
-
+import { API } from "./data.js";
+console.log("ROhit");
+// console.log(API);
 var myHeaders = new Headers();
-let key = "xauunvDe8N05BUCjZLPVgywDQh0FnsxT";
-myHeaders.append("apikey", key);
+myHeaders.append("apikey", "xauunvDe8N05BUCjZLPVgywDQh0FnsxT");
 
 var requestOptions = {
   method: "GET",
@@ -11,14 +10,7 @@ var requestOptions = {
   redirect: "follow",
 };
 
-fetch(
-  "https://api.tomorrow.io/v4/weather/realtime?location=ajmer",
-  requestOptions
-)
+fetch(`${API.APIURL}/realtime?location=ajmer`, requestOptions)
   .then((response) => response.json())
   .then((result) => console.log(result))
   .catch((error) => console.log("error", error));
-
-
-const apikey = process.env.VITE_API_KEY;
-console.log(apikey);
