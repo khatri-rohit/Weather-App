addf4e69cdf6b47912ce6b4a270c590c8536cf11
Rohit <rohitkhatri111112@gmail.com>
2024-05-17 02:15:50 +0530

    Commit

--- js/app1.js
+++ js/app1.js
@@ -1,40 +1,29 @@
-// const url =
-//   "https://place-autocomplete1.p.rapidapi.com/autocomplete/json?input=ind&radius=500";
-// const options = {
+// import { PLACE_API } from "./data.js";
+
+// let myHeaders = new Headers();
+// myHeaders.append("X-RapidAPI-Key", PLACE_API.APIKEY);
+
+// let requestOptions = {
 //   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "175a467426mshe8285a4a411f620p16192bjsna983e4d66aed",
-//     "X-RapidAPI-Host": "place-autocomplete1.p.rapidapi.com",
-//   },
+//   headers: myHeaders,
+//   redirect: "follow",
 // };
-// (async () => {
-//   try {
-//     const response = await fetch(url, options);
-//     const result = await response.text();
-//     console.log(result);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// })();
 
-const options = {
-  method: 'POST',
-  headers: {
-    accept: 'application/json',
-    'Accept-Encoding': 'gzip',
-    'content-type': 'application/json'
-  },
-  body: JSON.stringify({
-    location: '42.3478, -71.0466',
-    fields: ['temperatureAvg'],
-    timesteps: ['1d'],
-    startDate: '05-15',
-    endDate: '05-16',
-    units: 'metric'
-  })
-};
+// fetch(`${PLACE_API.APIURL}/json?input=jaipur&radius=500`, requestOptions)
+//   .then((response) => response.json())
+//   .then((result) => console.log(result))
+//   .catch((error) => console.log("error", error));
 
-fetch('https://api.tomorrow.io/v4/historical/normals?apikey=K4bouBhk9LCjqpsGp4Rrtl9VOliv14ka', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+async function gotLoaction(position) {
+  console.log(position.coords.latitude);
+  console.log(position.coords.longitude);
+}
+async function faliure() {
+  console.log("Error");
+}
+
+function loaction() {
+  navigator.geolocation.getCurrentPosition(gotLoaction, faliure);
+}
+
+loaction()
