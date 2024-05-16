1ae8e9b3115b368ec3bb293fdcc3a22d7ac4d213
Rohit <rohitkhatri111112@gmail.com>
2024-05-17 03:44:12 +0530

    Commit

--- js/app1.js
+++ js/app1.js
@@ -1,29 +1,60 @@
-// import { PLACE_API } from "./data.js";
+import { PLACE_API } from "./data.js";
 
-// let myHeaders = new Headers();
-// myHeaders.append("X-RapidAPI-Key", PLACE_API.APIKEY);
+const input = document.querySelector("input");
+const button = document.querySelector(".btn");
+const form = document.querySelector("form");
+const places = document.querySelector(".result-box");
 
-// let requestOptions = {
-//   method: "GET",
-//   headers: myHeaders,
-//   redirect: "follow",
-// };
+const myHeaders = new Headers();
+myHeaders.append("X-RapidAPI-Key", PLACE_API.APIKEY);
 
-// fetch(`${PLACE_API.APIURL}/json?input=jaipur&radius=500`, requestOptions)
-//   .then((response) => response.json())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.log("error", error));
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
 
-async function gotLoaction(position) {
-  console.log(position.coords.latitude);
-  console.log(position.coords.longitude);
-}
-async function faliure() {
-  console.log("Error");
-}
+input.addEventListener("keyup", async () => {
+  try {
+    const response = await fetch(
+      `${PLACE_API.APIURL}/json?input=${input.value}&radius=500`,
+      requestOptions
+    );
+    const result = await response.json();
+    places.style.display = "block";
+    for (let i = 0; i < result.predictions.length; i++) {
+      const place = document.createElement("p");
+      place.setAttribute("class", "place");
+      places.appendChild(place);
+    }
+    for (const key in result.predictions) {
+      if (Object.values(result.predictions, key)) {
+        const element = result.predictions[key];
+        const plac = document.querySelectorAll(".place");
+        plac[key].innerText = element.description;
+      }
+    }
+    if (input.value === "" || input.value === " ") {
+      places.style.display = "none";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
 
-function loaction() {
-  navigator.geolocation.getCurrentPosition(gotLoaction, faliure);
-}
+// async function gotLoaction(position) {
+//   console.log(position.coords.latitude);
+//   console.log(position.coords.longitude);
+// }
+// async function faliure() {
+//   console.log("Error");
+// }
 
-loaction()
+// function loaction() {
+//   navigator.geolocation.getCurrentPosition(gotLoaction, faliure);
+// }
+
+// loaction()
+
+// 26.4815994
+// 74.6048946
