264be66653e322e1e544a51aaaf1f87182c8c0c9
Rohit <rohitkhatri111112@gmail.com>
2024-05-18 01:46:29 +0530

    Commit

--- js/app.js
+++ js/app.js
@@ -5,6 +5,14 @@ const highestTemp = document.querySelector("#dayTem");
 const lowestTemp = document.querySelector("#nytTem");
 const temperature = document.querySelectorAll("#currTem");
 
+const nextTemp = document.querySelectorAll(".next-temp");
+const nextTiming = document.querySelectorAll(".timing");
+const humidity = document.querySelector(".hum");
+const dew = document.querySelector(".dew");
+const pressure = document.querySelector(".press");
+const UV = document.querySelector(".uv");
+const Visibility = document.querySelector(".visibi");
+
 const back = document.querySelector(".container");
 
 setInterval(() => {
@@ -19,18 +27,9 @@ setInterval(() => {
 
 const date = new Date();
 if (date.getHours() - 12 > 6) {
-  back.style.background = `url("../img/beautiful-clouds-digital-art.jpg")`;
+  back.style.background = `url("../img/nyt.jpg")`;
   back.style.backgroundRepeat = "no-repeat";
   back.style.backgroundSize = "cover";
-}
-
-const date1 = new Date("2024-05-17T17:00:00Z");
-console.log(date1.getHours() - 12);
-
-if (date1.getHours() > 12) {
-  console.log(date1.getHours() - 12);
-} else {
-  console.log(date1.getHours());
 }
 
 const myHeaders = new Headers();
@@ -48,26 +47,63 @@ async function todayTemperature() {
     requestOptions
   );
   const result = await response.json();
+
+  humidity.textContent = `${result.data.values.humidity}%`;
+  dew.textContent = `${result.data.values.dewPoint}℃`;
+  pressure.textContent = `${result.data.values.pressureSurfaceLevel} mBar`;
+  UV.textContent = `Low,${result.data.values.uvIndex}`;
+  Visibility.textContent = `${Math.round(result.data.values.visibility)}km`;
+
   let temp = result.data.values.temperature;
   return temp;
 }
 
-try {
-  const response = await fetch(
-    `${API.APIURL}/forecast?timesteps=1d&location=26.4815994,74.6048946`,
-    requestOptions
-  );
-  const result = await response.json();
-  let maxTemp = result.timelines.daily[0].values.temperatureMax;
-  let minTemp = result.timelines.daily[0].values.temperatureMin;
-  highestTemp.textContent = `${Math.round(maxTemp)}°`;
-  lowestTemp.textContent = `${Math.round(minTemp)}°`;
-  const todayTemp = todayTemperature();
-  todayTemp.then((res) => {
-    temperature.forEach((tem) => {
-      tem.textContent = Math.round(res);
+(async () => {
+  try {
+    const response = await fetch(
+      `${API.APIURL}/forecast?timesteps=1d&location=26.4815994,74.6048946`,
+      requestOptions
+    );
+    const result = await response.json();
+    let maxTemp = result.timelines.daily[0].values.temperatureMax;
+    let minTemp = result.timelines.daily[0].values.temperatureMin;
+    highestTemp.textContent = `${Math.round(maxTemp)}°`;
+    lowestTemp.textContent = `${Math.round(minTemp)}°`;
+    console.log("Called");
+    const todayTemp = todayTemperature();
+    todayTemp.then((res) => {
+      temperature.forEach((tem) => {
+        tem.textContent = Math.round(res);
+      });
     });
-  });
-} catch (error) {
-  console.error(error);
-}
+
+    const hourlyResponse = await fetch(
+      `${API.APIURL}/forecast?timesteps=1h&location=26.4815994,74.6048946`,
+      requestOptions
+    );
+    const hourlyResult = await hourlyResponse.json();
+    let arr = 1;
+    nextTemp.forEach((nxtTmp) => {
+      nxtTmp.textContent = `${Math.round(
+        hourlyResult.timelines.hourly[arr].values.temperature
+      )}°`;
+      arr++;
+    });
+    arr = 1;
+    nextTiming.forEach((nxtTime) => {
+      const date1 = new Date(hourlyResult.timelines.hourly[arr].time);
+      let time = date1.getHours();
+      var noon;
+      noon = "am";
+      if (time > 12) {
+        noon = "pm";
+        time -= 12;
+      }
+      if (time === 0) time += 12;
+      nxtTime.textContent = `${time}:00 ${noon}`;
+      arr++;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
