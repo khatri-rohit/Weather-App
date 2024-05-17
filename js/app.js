c63874b94bd90c70396d17163b04abfbf54af878
Rohit <rohitkhatri111112@gmail.com>
2024-05-17 20:11:11 +0530

    Commit

--- js/app.js
+++ js/app.js
@@ -27,42 +27,45 @@ setInterval(() => {
 });
 
 const date = new Date();
-if (date.getHours() > 19 ) {
+if (date.getHours() - 12 > 6) {
   background.style.background = `url("../img/beautiful-clouds-digital-art.jpg")`;
   background.style.backgroundRepeat = "no-repeat";
   background.style.backgroundSize = "cover";
+}
+
+const date1 = new Date("2024-05-17T24:00:00Z");
+if (date1.getHours() > 12) {
+  console.log(date1.getHours() - 12);
 } else {
-  background.style.background = `url("../img/mountains-with-cloudy-sky-surface.jpg")`;
-  background.style.backgroundRepeat = "no-repeat";
-  background.style.backgroundSize = "cover";
+  console.log(date1.getHours());
 }
 
-async function todayTemperature() {
-  const response = await fetch(
-    `${API.APIURL}/realtime?location=26.4815994,74.6048946`,
-    requestOptions
-  );
-  const result = await response.json();
-  let temp = result.data.values.temperature;
-  return temp;
-}
+// async function todayTemperature() {
+//   const response = await fetch(
+//     `${API.APIURL}/realtime?location=26.4815994,74.6048946`,
+//     requestOptions
+//   );
+//   const result = await response.json();
+//   let temp = result.data.values.temperature;
+//   return temp;
+// }
 
-try {
-  const response = await fetch(
-    `${API.APIURL}/forecast?timesteps=1d&location=26.4815994,74.6048946`,
-    requestOptions
-  );
-  const result = await response.json();
-  let maxTemp = result.timelines.daily[0].values.temperatureMax;
-  let minTemp = result.timelines.daily[0].values.temperatureMin;
-  highestTemp.textContent = `${Math.round(maxTemp)}°`;
-  lowestTemp.textContent = `${Math.round(minTemp)}°`;
-  const todayTemp = todayTemperature();
-  todayTemp.then((res) => {
-    temperature.forEach((tem) => {
-      tem.textContent = Math.round(res);
-    });
-  });
-} catch (error) {
-  console.error(error);
-}
+// try {
+//   const response = await fetch(
+//     `${API.APIURL}/forecast?timesteps=1d&location=26.4815994,74.6048946`,
+//     requestOptions
+//   );
+//   const result = await response.json();
+//   let maxTemp = result.timelines.daily[0].values.temperatureMax;
+//   let minTemp = result.timelines.daily[0].values.temperatureMin;
+//   highestTemp.textContent = `${Math.round(maxTemp)}°`;
+//   lowestTemp.textContent = `${Math.round(minTemp)}°`;
+//   const todayTemp = todayTemperature();
+//   todayTemp.then((res) => {
+//     temperature.forEach((tem) => {
+//       tem.textContent = Math.round(res);
+//     });
+//   });
+// } catch (error) {
+//   console.error(error);
+// }
