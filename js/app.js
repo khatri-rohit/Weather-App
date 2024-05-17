ba095df7131fa1e082f4191a7433ed41eb70f561
Rohit <rohitkhatri111112@gmail.com>
2024-05-17 22:23:29 +0530

    Commit

--- js/app.js
+++ js/app.js
@@ -5,16 +5,7 @@ const highestTemp = document.querySelector("#dayTem");
 const lowestTemp = document.querySelector("#nytTem");
 const temperature = document.querySelectorAll("#currTem");
 
-const background = document.querySelector(".container");
-
-const myHeaders = new Headers();
-myHeaders.append("apikey", API.APIKEY);
-
-const requestOptions = {
-  method: "GET",
-  headers: myHeaders,
-  redirect: "follow",
-};
+const back = document.querySelector(".container");
 
 setInterval(() => {
   const date = new Date();
@@ -28,17 +19,27 @@ setInterval(() => {
 
 const date = new Date();
 if (date.getHours() - 12 > 6) {
-  background.style.background = `url("../img/beautiful-clouds-digital-art.jpg")`;
-  background.style.backgroundRepeat = "no-repeat";
-  background.style.backgroundSize = "cover";
+  back.style.background = `url("../img/beautiful-clouds-digital-art.jpg")`;
+  back.style.backgroundRepeat = "no-repeat";
+  back.style.backgroundSize = "cover";
 }
 
-const date1 = new Date("2024-05-17T24:00:00Z");
-if (date1.getHours() > 12) {
-  console.log(date1.getHours() - 12);
-} else {
-  console.log(date1.getHours());
-}
+const date1 = new Date("2024-05-17T15:00:00Z");
+console.log(date1.getHours());
+
+// if (date1.getHours() > 12) {
+// console.log(date1.getHours() - 12);
+// } else {
+// }
+
+const myHeaders = new Headers();
+myHeaders.append("apikey", API.APIKEY);
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow",
+};
 
 // async function todayTemperature() {
 //   const response = await fetch(
