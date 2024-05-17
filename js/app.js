e128c79f8945cfe6c5e482c38a45a0c24d177946
Rohit <rohitkhatri111112@gmail.com>
2024-05-17 17:31:15 +0530

    Commit

--- js/app.js
+++ js/app.js
@@ -1,15 +1,68 @@
 import { API } from "./data.js";
-var myHeaders = new Headers();
+
+const today = document.querySelector(".date");
+const highestTemp = document.querySelector("#dayTem");
+const lowestTemp = document.querySelector("#nytTem");
+const temperature = document.querySelectorAll("#currTem");
+
+const background = document.querySelector(".container");
+
+const myHeaders = new Headers();
 myHeaders.append("apikey", API.APIKEY);
 
-let requestOptions = {
+const requestOptions = {
   method: "GET",
   headers: myHeaders,
   redirect: "follow",
 };
 
+setInterval(() => {
+  const date = new Date();
+  let setDate = `${date.getDate()} ${date.toLocaleTimeString("en-us", {
+    month: "short",
+    hour: "2-digit",
+    minute: "2-digit",
+  })}`;
+  today.textContent = setDate;
+});
 
-fetch(`${API.APIURL}/realtime?location=Indore, Madhya Pradesh, India`, requestOptions)
-  .then((response) => response.json())
-  .then((result) => console.log(result))
-  .catch((error) => console.log("error", error));
+const date = new Date();
+if (date.getHours() - 12  > 7 ) {
+  background.style.background = `url("../img/beautiful-clouds-digital-art.jpg")`;
+  background.style.backgroundRepeat = "no-repeat";
+  background.style.backgroundSize = "cover";
+} else {
+  background.style.background = `url("../img/mountains-with-cloudy-sky-surface.jpg")`;
+  background.style.backgroundRepeat = "no-repeat";
+  background.style.backgroundSize = "cover";
+}
+
+async function todayTemperature() {
+  const response = await fetch(
+    `${API.APIURL}/realtime?location=26.4815994,74.6048946`,
+    requestOptions
+  );
+  const result = await response.json();
+  let temp = result.data.values.temperature;
+  return temp;
+}
+
+try {
+  const response = await fetch(
+    `${API.APIURL}/forecast?timesteps=1d&location=26.4815994,74.6048946`,
+    requestOptions
+  );
+  const result = await response.json();
+  let maxTemp = result.timelines.daily[0].values.temperatureMax;
+  let minTemp = result.timelines.daily[0].values.temperatureMin;
+  highestTemp.textContent = `${Math.round(maxTemp)}°`;
+  lowestTemp.textContent = `${Math.round(minTemp)}°`;
+  const todayTemp = todayTemperature();
+  todayTemp.then((res) => {
+    temperature.forEach((tem) => {
+      tem.textContent = Math.round(res);
+    });
+  });
+} catch (error) {
+  console.error(error);
+}
